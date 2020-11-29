e9a0ce09e38defab778d312ee08390d6ccce27e5
unknown <julzllanillo@gmail.com>
2020-11-29 20:51:33 +0800

    Fixed styling. Added size picker. Removed scaling.

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const colorpicker = document.getElementById("colorpicker");
 const sizepicker = document.getElementById("sizepicker");
-const boardsizepicker = document.getElementById("boardsizepicker");
+const resoPickerDiv = document.getElementById("resoPicker");
+const initReso = document.getElementById("submitReso");
+const widthPicker = document.getElementById("widthPicker");
+const heightPicker = document.getElementById("heightPicker");
 const canvas = document.getElementById("board");
 const saveBtn = document.getElementById("saveButton");
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = 8;
-const CANVAS_HEIGHT = 8;
 
 const tempCanvas = document.createElement("canvas");
 const tempCtx = tempCanvas.getContext('2d');
 
-let scale = 10;
+let CANVAS_WIDTH = 8;
+let CANVAS_HEIGHT = 8;
+let scale = Math.floor(window.innerWidth / (CANVAS_WIDTH * 2.5));
+let currentWidth = CANVAS_WIDTH * scale;
+let currentHeight = CANVAS_HEIGHT * scale;
+
 let color = colorpicker.value;
 let size = sizepicker.value * scale;
+
 let isPressed = false;
-let currentX = 0; 
+let currentX = 0;
 let currentY = 0;
 
 canvas.width = CANVAS_WIDTH * scale;
@@ -22,9 +29,28 @@ canvas.height = CANVAS_HEIGHT * scale;
 canvas.style.width = `${CANVAS_WIDTH * scale}px`
 canvas.style.height = `${CANVAS_HEIGHT * scale}px`
 
-ctx.webkitImageSmoothingEnabled = false;
-ctx.mozImageSmoothingEnabled = false;
+ctx.mozImageSmoothingEnabled = false;  // firefox
 ctx.imageSmoothingEnabled = false;
+
+initReso.onclick = function (e) {
+    CANVAS_WIDTH = parseInt(widthPicker.value);
+    CANVAS_HEIGHT = parseInt(heightPicker.value);
+
+    scale = Math.floor(window.innerWidth / (CANVAS_WIDTH * 2.5));
+    color = colorpicker.value;
+    size = sizepicker.value * scale;
+
+    currentWidth = CANVAS_WIDTH * scale;
+    currentHeight = CANVAS_HEIGHT * scale;
+    canvas.width = CANVAS_WIDTH * scale;
+    canvas.height = CANVAS_HEIGHT * scale;
+    canvas.style.width = `${CANVAS_WIDTH * scale}px`;
+    canvas.style.height = `${CANVAS_HEIGHT * scale}px`;
+
+    resoPickerDiv.style.display = "none";
+    draw();
+    
+}
 
 window.onmousedown = function() {
     isPressed = true;
@@ -42,37 +68,12 @@ sizepicker.oninput = function() {
     size = this.value * scale;
 }
 
-
-function drawOnTempCanvas(canvas, width, height, scale) {
-    tempCanvas.width = width;
-    tempCanvas.height = height;
-    tempCtx.drawImage(canvas, 0, 0, width * scale, height * scale, 0, 0, width, height);
-}
-
-
-boardsizepicker.oninput = function() {
-    let newCanvasWidth =  CANVAS_WIDTH * scale;
-    let newCanvasHeight =  CANVAS_HEIGHT * scale;
-
-    tempCanvas.width = newCanvasWidth;
-    tempCanvas.height = newCanvasHeight;
-    tempCtx.drawImage(canvas, 0, 0);
-
-    scale = this.value;
-    size = sizepicker.value * scale;
-
-    canvas.width = newCanvasWidth;
-    canvas.height = newCanvasHeight;
-    canvas.style.width = `${newCanvasWidth}px`
-    canvas.style.height = `${newCanvasHeight}px`
-
-    // TODO: Fix redraw when resizing
-    ctx.drawImage(tempCanvas, 0, 0);
-}
-
-
 saveBtn.addEventListener('click', function (event) {
-    drawOnTempCanvas(canvas, CANVAS_WIDTH, CANVAS_HEIGHT, scale);
+    tempCtx.mozImageSmoothingEnabled = false;  // firefox
+    tempCtx.imageSmoothingEnabled = false;
+    tempCanvas.width = CANVAS_WIDTH;
+    tempCanvas.height = CANVAS_HEIGHT;
+    tempCtx.drawImage(canvas, 0, 0, CANVAS_WIDTH * scale, CANVAS_HEIGHT * scale, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     let download = document.getElementById("downloadLink");
     let image = tempCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
 
@@ -102,8 +103,6 @@ function draw(event) {
 }
 
 
-canvas.onmousemove = function (event) {
+window.onmousemove = function (event) {
     getMousePos(canvas, event);
 }
-
-draw();
